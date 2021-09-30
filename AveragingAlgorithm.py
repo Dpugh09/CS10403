789d481846301dfe37365d62cdb98ecf486b10c9
Dpugh09 <91210753+Dpugh09@users.noreply.github.com>
2021-09-30 12:42:45 -0400

    This version 1 of my averaging alogorithm

---
+++ AveragingAlgorithm.py
@@ -0,0 +1,12 @@
+average = 0
+total = 0
+howManyEntered = 0
+print('How many test scores would you like to average')
+howMany = int(input(''))
+while howManyEntered < howMany:
+    print('Enter test score:')
+    testScore = int(input(''))
+    total = total + testScore
+    howManyEntered = howManyEntered + 1
+average = total / howMany
+print('The average for test scores you entered is: ',average)
