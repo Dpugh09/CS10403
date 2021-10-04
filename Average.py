d8144e132d226ac7f16a2114d5d720b6eef79396
Dpugh09 <91210753+Dpugh09@users.noreply.github.com>
2021-10-04 12:13:49 -0400

    Update Average.py

--- Average.py
+++ Average.py
@@ -1,8 +1,7 @@
 average = 0
 total = 0
 howManyEntered = 0
-print('How many test scores would you like to average')
-howMany = int(input(''))
+howMany = int(input('How many test scores would you like to average'))
 while howManyEntered < howMany:
     print('Enter test score:')
     testScore = int(input(''))
